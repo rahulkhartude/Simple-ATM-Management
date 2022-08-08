4091340627fa0a81c38e169047d12e9da3677294
rahulkhartude <90925722+rahulkhartude@users.noreply.github.com>
2022-08-08 08:12:58 +0530

    Add files via upload

---
+++ AsyncAwait.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncAwait
+{
+    internal class AsyncAwait
+    {
+        
+            static void Main(string[] args)
+            {
+                Method1();
+                Method2();
+                Console.ReadKey();
+            }
+
+            public static async Task Method1()
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine(" Method 1");
+                        
+
+                    }
+                });
+            }
+
+
+            public static void Method2()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(" Method 2");
+                    
+
+                }
+            }
+        
+    }
+}
---
+++ FileIO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    internal class FileIO
+    {
+        static void Main(string[] args)
+        {
+            FileStream F = new FileStream("test1.dat", FileMode.OpenOrCreate,
+             FileAccess.ReadWrite);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                F.WriteByte((byte)i);
+            }
+            F.Position = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.Write(F.ReadByte() + " ");
+            }
+            F.Close();
+            Console.ReadKey();
+        }
+    }
+}
